2ac3f0339d85bb03d6307516d79c40a793eba4cd
David <davidraleigh@gmail.com>
2013-02-07 19:06:06 +0100

    Fixed apple's junky constraints bugs.

--- iOS/LiarsDice/LiarsDice/AppDelegate.m
+++ iOS/LiarsDice/LiarsDice/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "StartViewController_iPhone.h"
+#import "RoundPlayViewController_iPhone.h"
 
 @implementation AppDelegate
 
@@ -17,6 +18,7 @@
     // Override point for customization after application launch.
     
     StartViewController_iPhone  *svc_i = [[StartViewController_iPhone alloc] init];
+    //RoundPlayViewController_iPhone *svc_i = [[RoundPlayViewController_iPhone alloc] init];
     
     UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:svc_i];
     navController.navigationBar.hidden = YES;
